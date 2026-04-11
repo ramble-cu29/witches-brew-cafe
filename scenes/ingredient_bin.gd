7d5b25bdad2d55482084797e3d2707416db70672
Ramble <rambleinthebramble@proton.me>
2026-04-10 20:37:14 -0800

    Ingredients bin with ingredient appearing when clicked

---
+++ scenes/ingredient_bin.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+signal clicked
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if is_pixel_opaque(get_local_mouse_position()):
+				clicked.emit()
+	
